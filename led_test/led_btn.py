3a1cd87f2e68da5e7e943f9350975f0fa4c5b2a3
CoolDong <32934089+Lcooldong@users.noreply.github.com>
2021-09-26 20:14:39 +0900

    Update led_btn.py

--- led_test/led_btn.py
+++ led_test/led_btn.py
@@ -2,12 +2,14 @@ import RPi.GPIO as GPIO
 
 led1 = 18 # P1 pin 12
 led2 = 23 # P1 pin 16
-btn  = 17 # P1 pin 11
+btn1  = 24 # P1 pin 11
+btn2  = 25 # P1 pin 11
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led1, GPIO.OUT)
 GPIO.setup(led2, GPIO.OUT)
-GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(btn1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(btn2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 print("Ctrl + C to exit")
 try:
