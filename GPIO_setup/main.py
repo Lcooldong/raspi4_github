cbe7838bc0252e2774099c763bfdabc83e281612
CoolDong <n_cooldong@naver.com>
2021-10-08 22:54:20 +0900

    Update main.py

--- GPIO_setup/main.py
+++ GPIO_setup/main.py
@@ -1,4 +1,3 @@
-
 import RPi.GPIO as GPIO
 import time
 
@@ -23,29 +22,41 @@ GPIO.setup(ledRed, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(btn1, GPIO.IN)
 GPIO.setup(btn2, GPIO.IN)
 
+btn1State = False
+lastBtn1State = False
+btn2State = False
+lastBtn2State = False
+
+
 # loop
 if __name__ == '__main__':
     print("Ctrl + C to exit")
     try:
         while True:
-            btn1Read = GPIO.input(btn1)
-            btn2Read = GPIO.input(btn2)
+            btn1State = GPIO.input(btn1)
+            btn2State = GPIO.input(btn2)
 
-            if (btn1Read == True):
+            if (btn1State != lastBtn1State):
                 print("btn1")
-                time.sleep(0.5)
                 GPIO.output(ledGreen, GPIO.HIGH)
-            # else:
-            #     GPIO.output(UVLed, GPIO.HIGH)
-            #     time.sleep(0.1)
-            #     GPIO.output(UVLed, GPIO.LOW)
-            #     time.sleep(0.1)
+                time.sleep(1)
+                GPIO.output(ledGreen, GPIO.LOW)
 
-            if (btn2Read == True):
+            if (btn2State != lastBtn2State):
                 print("btn2")
-                time.sleep(0.5)
                 GPIO.output(ledRed, GPIO.HIGH)
+                time.sleep(1)
+                GPIO.output(ledRed, GPIO.LOW)
 
+            if (btn1State == True):
+                GPIO.output(whiteLed, GPIO.HIGH)
+                time.sleep(0.5)
+                GPIO.output(whiteLed, GPIO.LOW)
+                time.sleep(0.5)
+
+            time.sleep(0.5)
+            lastBtn1State = btn1State
+            lastBtn2State = btn2State
 
     except:
         print("error")
