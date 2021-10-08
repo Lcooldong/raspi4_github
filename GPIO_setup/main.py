230335b3f1d390e1e5ed365f9e240a09d692a0c8
CoolDong <n_cooldong@naver.com>
2021-10-08 21:36:06 +0900

    Update main.py

--- GPIO_setup/main.py
+++ GPIO_setup/main.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
             # print(btn2Read)
             # time.sleep(1)
 
-            if (btn1Read == False):
+            if (btn1Read == True):
                 print("btn1")
                 time.sleep(0.5)
             else:
