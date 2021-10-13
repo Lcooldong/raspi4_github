42996f35046d7932d11a9967a0cd5a48414dfaf7
CoolDong <n_cooldong@naver.com>
2021-10-13 16:17:56 +0900

    Update implement.py

--- Proto_01/function/implement.py
+++ Proto_01/function/implement.py
@@ -5,23 +5,23 @@ from setup import *
 
 def start_program():
     GPIO.output(vibrator, GPIO.HIGH)
-    time.sleep(0.5)
+    time.sleep(1)
     GPIO.output(vibrator, GPIO.LOW)
     GPIO.output(buzzer, GPIO.HIGH)
-    time.sleep(0.1)
+    time.sleep(0.5)
     GPIO.output(buzzer, GPIO.LOW)
 
 def detect_blood():
     GPIO.output(led_Green, GPIO.HIGH)
     GPIO.output(vibrator, GPIO.HIGH)
     GPIO.output(buzzer, GPIO.HIGH)
-    time.sleep(0.3)
+    time.sleep(0.5)
     GPIO.output(vibrator, GPIO.LOW)
     GPIO.output(buzzer, GPIO.LOW)
-    time.sleep(0.3)
+    time.sleep(0.5)
     GPIO.output(vibrator, GPIO.HIGH)
     GPIO.output(buzzer, GPIO.HIGH)
-    time.sleep(0.3)
+    time.sleep(0.5)
     GPIO.output(vibrator, GPIO.LOW)
     GPIO.output(buzzer, GPIO.LOW)
     GPIO.output(led_Green, GPIO.LOW)
