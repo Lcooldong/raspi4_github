6fa1120acdab6ef7778338564abe1002d1237d96
CoolDong <n_cooldong@naver.com>
2021-10-13 17:02:36 +0900

    Update main.py

--- Proto_01/main.py
+++ Proto_01/main.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     try:
         GPIO.add_event_detect(btn1, GPIO.RISING, callback=button_pressed_callback, bouncetime=1000)
         #signal.signal(signal.SIGINT, signal_handler)
-        #signal.pause()
+        signal.pause()
 
     except KeyboardInterrupt:
         print("error")
