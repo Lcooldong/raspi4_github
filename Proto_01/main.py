109d5098ba4dc5ca4978b99a9c5e337a211c7c06
CoolDong <n_cooldong@naver.com>
2021-10-13 15:20:45 +0900

    Update main.py

--- Proto_01/main.py
+++ Proto_01/main.py
@@ -61,6 +61,14 @@ def button_pressed_callback(channel):
 # MAIN LOOP
 if __name__ == '__main__':
     try:
+        GPIO.output(vibrator, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(vibrator, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(vibrator, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(vibrator, GPIO.HIGH)
+        time.sleep(1)
         GPIO.add_event_detect(btn1, GPIO.RISING, callback=button_pressed_callback, bouncetime=1000)
         signal.signal(signal.SIGINT, signal_handler)
         signal.pause()
