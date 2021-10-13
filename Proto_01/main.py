ce605c888777cedd80cb23c093af869ff89e9fed
CoolDong <n_cooldong@naver.com>
2021-10-13 16:59:23 +0900

    Update main.py

--- Proto_01/main.py
+++ Proto_01/main.py
@@ -10,10 +10,19 @@ def signal_handler(sig, frame):
     GPIO.cleanup()
     sys.exit(0)
 
+last_ledGreen_state = 0
+count = 0
 def button_pressed_callback(channel):
-    global state
-    detect_blood()
-    print("detect blood!")
+    global last_ledGreen_state
+    global count
+    GPIO.output(led_Green, not last_ledGreen_state)
+    last_ledGreen_state = not last_ledGreen_state
+    #print("btn1")
+    count += 1
+    print(count)
+    # global state
+    # detect_blood()
+    # print("detect blood!")
 
 
 # MAIN LOOP
