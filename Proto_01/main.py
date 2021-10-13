736d3ae4a5ea319fd6355b36afd75b4f315d3674
CoolDong <n_cooldong@naver.com>
2021-10-13 16:40:28 +0900

    Update main.py

--- Proto_01/main.py
+++ Proto_01/main.py
@@ -13,9 +13,6 @@ def button_pressed_callback(channel):
     if state == 0:
         detect_blood()
         state = 1
-    elif state == 1:
-        detect_stain()
-        state = 0
     else:
         GPIO.output(led_White, GPIO.HIGH)
 
