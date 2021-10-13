1192af1f1aae4c11d24e337817f6c4ff990be0c5
CoolDong <n_cooldong@naver.com>
2021-10-13 15:10:51 +0900

    proto

--- Proto_01/main.py
+++ Proto_01/main.py
@@ -3,20 +3,37 @@ import sys
 import RPi.GPIO as GPIO
 import time
 
+#INPUT
 btn1 = 18
+
+#RGB
 led_Blue = 22
 led_Green = 27
 led_Red = 17
 
+#Peripheral
+led_UV = 12
+led_White = 16
+vibrator = 20
+buzzer = 21
 
-last_ledGreen_state = 0
-count = 0
-
+# SETUP
 GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(btn1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 GPIO.setup(led_Blue, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(led_Green, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(led_Red, GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.setup(led_UV, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(led_White, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(vibrator, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(buzzer, GPIO.OUT, initial=GPIO.LOW)
+
+
+last_ledGreen_state = 0
+count = 0
 
 def signal_handler(sig, frame):
     GPIO.cleanup()
@@ -32,7 +49,7 @@ def button_pressed_callback1(channel):
     count += 1
     print(count)
 
-
+# MAIN LOOP
 if __name__ == '__main__':
     try:
         while True:
