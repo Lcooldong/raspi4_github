3d6a658b2940b37783bb94556801add1baa4a18d
CoolDong <n_cooldong@naver.com>
2021-10-14 19:32:26 +0900

    Update main.py

--- opencv_test/main.py
+++ opencv_test/main.py
@@ -3,7 +3,7 @@ import cv2
 
 
 if __name__ == '__main__':
-    cap = cv2.VideoCapture(0)  # 노트북 웹캠을 카메라로 사용
+    cap = cv2.VideoCapture(-1)  # 노트북 웹캠을 카메라로 사용
     cap.set(3, 640)  # 너비
     cap.set(4, 480)  # 높이
 
